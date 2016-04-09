0033951e6871fd93cd139669e1546e117e71a20d
Nikonenko <mexahik3333@gmail.com>
2016-04-09 20:15:11 +0800

    Проверка на существующих пользователей

--- web/index.php
+++ web/index.php
@@ -5,3 +5,57 @@
  * Date: 09.04.2016
  * Time: 15:47
  */
+
+  
+    session_start();
+    ?>
+<html>
+<head>
+    <title>Главная страница</title>
+</head>
+<body>
+<h2>Главная страница</h2>
+<form action="testreg.php" method="post">
+
+   
+    <p>
+        <label>Ваш логин:<br></label>
+        <input name="login" type="text" size="15" maxlength="15">
+    </p>
+
+
+   
+
+    <p>
+
+        <label>Ваш пароль:<br></label>
+        <input name="password" type="password" size="15" maxlength="15">
+    </p>
+
+   
+
+    <p>
+        <input type="submit" name="submit" value="Войти">
+
+      
+        <br>
+      
+        <a href="reg.php">Зарегистрироваться</a>
+    </p></form>
+<br>
+<?php
+
+if (empty($_SESSION['login']) or empty($_SESSION['id']))
+{
+    
+    echo "Вы вошли на сайт, как гость";
+}
+else
+{
+
+    
+    echo "Вы вошли на сайт, как ".$_SESSION['login'];
+}
+?>
+</body>
+</html>
--- web/save_user.php
+++ web/save_user.php
@@ -7,15 +7,15 @@
  */
 if (isset($_POST['name'])) { $name = $_POST['name']; if ($name == '') { unset($name);}}
 if (isset($_POST['last_name'])) { $last_name = $_POST['last_name']; if ($last_name == '') { unset($last_name);}}
-if (isset($_POST['login'])) { $login = $_POST['login']; if ($login == '') { unset($login);}}  //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
+if (isset($_POST['login'])) { $login = $_POST['login']; if ($login == '') { unset($login);}}
     if (isset($_POST['email'])) { $email = $_POST['email']; if ($email == '') { unset($email);}}
     if (isset($_POST['password'])) { $password=$_POST['password']; if ($password =='') { unset($password);} }
-//заносим введенный пользователем пароль в переменную $password, если он пустой, то уничтожаем переменную
-if (empty($login) or empty($password) or empty($name) or empty($last_name) or empty($email)) //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
+
+if (empty($login) or empty($password) or empty($name) or empty($last_name) or empty($email))
 {
     exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
 }
-//если логин и пароль введены, то обрабатываем их, чтобы теги и скрипты не работали, мало ли что люди могут ввести
+
 $login = stripslashes($login);
 $login = htmlspecialchars($login);
 $password = stripslashes($password);
@@ -26,24 +26,24 @@ $last_name = stripslashes($last_name);
 $last_name = htmlspecialchars($last_name);
 $email = stripslashes($email);
 $email = htmlspecialchars($email);
-//удаляем лишние пробелы
+
 $login = trim($login);
 $password = trim($password);
 $name = trim($name);
 $last_name = trim($last_name);
 $email = trim($email);
 
-// подключаемся к базе
-include ("bd.php");// файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь
-// проверка на существование пользователя с таким же логином
+
+include ("bd.php");
+
 $result = mysql_query("SELECT id FROM reg WHERE login='$login'",$db);
 $myrow = mysql_fetch_array($result);
 if (!empty($myrow['id'])) {
     exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
 }
-// если такого нет, то сохраняем данные
-$result2 = mysql_query ("INSERT INTO reg (login,password) VALUES('$login','$password')");
-// Проверяем, есть ли ошибки
+
+$result2 = mysql_query ("INSERT INTO reg (name, last_name, email, login, password) VALUES('$login','$password','$name','$last_name','$email')");
+
 if ($result2=='TRUE')
 {
     echo "Вы успешно зарегистрированы! Теперь вы можете зайти на сайт. <a href='index.php'>Главная страница</a>";
