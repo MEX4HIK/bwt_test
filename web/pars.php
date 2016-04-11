a2741eb68992c4b289e7373fa298f38501073f70
Nikonenko <mexahik3333@gmail.com>
2016-04-11 21:38:32 +0800

    создана страничка для парсинга погоды, и попытки парсить страничку

---
+++ web/pars.php
@@ -0,0 +1,66 @@
+<html>
+<meta charset="utf-8">
+</html>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nikonenko
+ * Date: 11.04.2016
+ * Time: 20:52
+ */
+#откуда будем парсить информацию:
+/*$gismeteo = 'http://www.gismeteo.ua/city/daily/5093/';
+$content = file_get_contents ($gismeteo);
+preg_match_all ('#<div class="pampers">(.*?)<ul class="mnu">#is', $content,$pogoda);
+var_dump($pogoda[0]);
+?>*/
+
+
+
+/*$lurl=get_fcontent("http://www.gismeteo.ua/city/daily/5093/");
+echo"cid:".$lurl[0]."<BR>";
+
+
+function get_fcontent( $url,  $javascript_loop = 0, $timeout = 5 ) {
+    $url = str_replace( "&amp;", "&", urldecode(trim($url)) );
+
+    $cookie = tempnam ("/tmp", "CURLCOOKIE");
+    $ch = curl_init();
+    curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1" );
+    curl_setopt( $ch, CURLOPT_URL, $url );
+    curl_setopt( $ch, CURLOPT_COOKIEJAR, $cookie );
+    curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
+    curl_setopt( $ch, CURLOPT_ENCODING, "" );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+    curl_setopt( $ch, CURLOPT_AUTOREFERER, true );
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );    # required for https urls
+    curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
+    curl_setopt( $ch, CURLOPT_TIMEOUT, $timeout );
+    curl_setopt( $ch, CURLOPT_MAXREDIRS, 10 );
+    $content = curl_exec( $ch );
+    $response = curl_getinfo( $ch );
+    curl_close ( $ch );
+
+    if ($response['http_code'] == 301 || $response['http_code'] == 302) {
+        ini_set("user_agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1");
+
+        if ( $headers = get_headers($response['url']) ) {
+            foreach( $headers as $value ) {
+                if ( substr( strtolower($value), 0, 9 ) == "location:" )
+                    return get_url( trim( substr( $value, 9, strlen($value) ) ) );
+            }
+        }
+    }
+
+    if (    ( preg_match("/>[[:space:]]+window\.location\.replace\('(.*)'\)/i", $content, $value) || preg_match("/>[[:space:]]+window\.location\=\"(.*)\"/i", $content, $value) ) && $javascript_loop < 5) {
+        return get_url( $value[1], $javascript_loop+1 );
+    } else {
+        return array( $content, $response );
+    }
+}
+?>*/
+
+
+/*$homepage = file_get_contents('http://www.gismeteo.ua/city/daily/5093/');
+echo $homepage;*/
+?>
