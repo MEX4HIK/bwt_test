ed4c5a4be6a2f2082ae587ca8df65422d9e5b3fe
Nikonenko <mexahik3333@gmail.com>
2016-04-14 16:47:50 +0800

    маленький фикс, форма с логином теперь не появляется когда вы уже зарегистрированы

--- web/index.php
+++ web/index.php
@@ -16,7 +16,36 @@
 </head>
 <body>
 <h2>Главная страница</h2>
-<form action="testreg.php" method="post">
+<?php
+if (empty($_SESSION['login']) or empty($_SESSION['id'])){
+echo "<html>
+    <form action=\"testreg.php\" method=\"post\">
+
+   
+    <p>
+        <label>Ваш логин:<br></label>
+        <input name=\"login\" type=\"text\" size=\"15\" maxlength=\"15\">
+    </p>
+
+    <p>
+
+        <label>Ваш пароль:<br></label>
+        <input name=\"password\" type=\"password\" size=\"15\" maxlength=\"15\">
+    </p>
+    <p>
+        <input type=\"submit\" name=\"submit\" value=\"Войти\">
+        <br>
+
+        <a href=\"reg.php\">Зарегистрироваться</a>
+    </p></form>
+
+<br>
+</html>";
+}
+?>
+
+<!--<form action="testreg.php" method="post">
+
 
    
     <p>
@@ -37,7 +66,7 @@
     </p></form>
 
 <br>
-
+-->
 <?php
 if (isset($_REQUEST['submitData']))
 {session_start();
