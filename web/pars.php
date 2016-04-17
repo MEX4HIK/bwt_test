3cfb3a648d9bf8da772dae3098eaf0c6fedeb4be
Nikonenko <mexahik3333@gmail.com>
2016-04-17 17:31:12 +0800

    вызов функции для вывода меню на страницу

--- web/pars.php
+++ web/pars.php
@@ -1,3 +1,8 @@
+<?php
+include("func_menu.php");
+$array2=[['index.php','Главная страница'],['pars.php', 'Погода'],['feedback.php','Обратная связь'],['list_feedback.php','Список отзывов']];
+echo menu($array2);
+?>
 <html>
 <meta charset="utf-8">
 <!-- Bootstrap-->
@@ -23,55 +28,8 @@
 $gismeteo = 'http://www.gismeteo.ua/city/daily/5093/';
 $content = file_get_contents ($gismeteo);
 preg_match_all ('#<div class="wsection wdata">(.*?)<div class="section bottom">#is', $content,$pogoda);
-//var_dump($pogoda[0]);
-//print_r ($pogoda[0]);
 echo $pogoda[0][0];
-/*$a = ['test' => [['lol', 'lol2', 'lol3' => 'lol4'], 'lol5']];
 
-echo $a['test'][0]['lol3'] ;
-echo $a['test'][1];*/
-
-/*$lurl=get_fcontent("http://www.gismeteo.ua/city/daily/5093/");
-echo"cid:".$lurl[0]."<BR>";
-
-
-function get_fcontent( $url,  $javascript_loop = 0, $timeout = 5 ) {
-    $url = str_replace( "&amp;", "&", urldecode(trim($url)) );
-
-    $cookie = tempnam ("/tmp", "CURLCOOKIE");
-    $ch = curl_init();
-    curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1" );
-    curl_setopt( $ch, CURLOPT_URL, $url );
-    curl_setopt( $ch, CURLOPT_COOKIEJAR, $cookie );
-    curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
-    curl_setopt( $ch, CURLOPT_ENCODING, "" );
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-    curl_setopt( $ch, CURLOPT_AUTOREFERER, true );
-    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );    # required for https urls
-    curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
-    curl_setopt( $ch, CURLOPT_TIMEOUT, $timeout );
-    curl_setopt( $ch, CURLOPT_MAXREDIRS, 10 );
-    $content = curl_exec( $ch );
-    $response = curl_getinfo( $ch );
-    curl_close ( $ch );
-
-    if ($response['http_code'] == 301 || $response['http_code'] == 302) {
-        ini_set("user_agent", "Mozilla/5.0 (Windows; U; Windows NT 5.1; rv:1.7.3) Gecko/20041001 Firefox/0.10.1");
-
-        if ( $headers = get_headers($response['url']) ) {
-            foreach( $headers as $value ) {
-                if ( substr( strtolower($value), 0, 9 ) == "location:" )
-                    return get_url( trim( substr( $value, 9, strlen($value) ) ) );
-            }
-        }
-    }
-
-    if (    ( preg_match("/>[[:space:]]+window\.location\.replace\('(.*)'\)/i", $content, $value) || preg_match("/>[[:space:]]+window\.location\=\"(.*)\"/i", $content, $value) ) && $javascript_loop < 5) {
-        return get_url( $value[1], $javascript_loop+1 );
-    } else {
-        return array( $content, $response );
-    }
-}*/
 
 ?>
 
