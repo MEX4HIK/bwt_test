e742169806ba05c240ee29b81eccba59e991c401
Nikonenko <mexahik3333@gmail.com>
2016-04-17 17:28:27 +0800

    страница переписана с вызовом методов для вывода меню

--- web/index.php
+++ web/index.php
@@ -8,7 +8,9 @@
 
   
     session_start();
-    ?>
+include ("func_menu.php");
+?>
+
 <html>
 <head>
     <meta charset="utf-8">
@@ -16,26 +18,14 @@
     <!-- Bootstrap-->
 
     <link href = "css/bootstrap.css" rel="stylesheet">
-    <link href = "css/style.css" rel="stylesheet">
 </head>
 <body>
-
-<!-- <div class="container">
-     <div class="row">
-         <h2>Главная страница</h2>
-         <div class="navbar navbar.inverse">
-             <div class="container">
-                 <div class="navbar-header">
-
-                 </div>
-                 <div class="collapse navbar-collapse" id="responsiv-menu">
-                     <ul class="nav navbar-nav">
-                         <li>  -->
 
 
 <?php
 
-/*выход пользователя из сессии */
+
+
 if (isset($_REQUEST['submitData']))
 {session_start();
     unset($_SESSION['password']);
@@ -44,14 +34,16 @@ if (isset($_REQUEST['submitData']))
     exit("<html><head><meta    http-equiv='Refresh' content='0;    URL=index.php'></head></html>");;}//в этом примере по нажатии кнопки выводится текст, может быть любой код.
 ?>
 
-
 <?php
+    if (empty($_SESSION['login']) or empty($_SESSION['id'])){
+        $array1=[['index.php','Главная страница'],['feedback.php','Обратная связь']];
+        echo menu($array1);
 
-if (empty($_SESSION['login']) or empty($_SESSION['id'])){
-    echo "<html>
-        
-     
+         echo "
+       
+   
     <div class=\"navbar-header\">
+   
     <form role=\"form\" action=\"testreg.php\" method=\"post\">
     <div class=\"form-group\">
     <label for=\"login\">Введите логин</label><input type=\"text\" name=\"login\" class=\"form-control\" id=\"login\">
@@ -63,40 +55,10 @@ if (empty($_SESSION['login']) or empty($_SESSION['id'])){
    
     <a href=\"reg.php\">Зарегистрироваться</a>
     </form>
-    </div>
+    </div>";
 
 
 
-</html>";
-}
-
-
-if (empty($_SESSION['login']) or empty($_SESSION['id']))
-{
-    
-
-    //bootstrap menu
-    echo "<html>
-    <div class=\"container\">
-     <div class=\"row\">
-         <h2>Главная страница</h2>
-         Вы вошли на сайт, как гость
-         <div class=\"navbar navbar-inverse\">
-             <div class=\"container\">
-                 <div class=\"navbar-header\">
-                 </div>
-                 <div class=\"collapse navbar-collapse\" id=\"responsiv-menu\">
-                     <ul class=\"nav navbar-nav\">
-                      <li> <br><a href=\"feedback.php\"><br>Обратная связь</a>
-                  </ul> 
-                  </div>
-                  </div>
-                  </div>
-                  </div>
-                  </div>
-    </html>";
-
-    //echo "<br><a href=\"feedback.php\"><br>Обратная связь</a>";
 }
 else
 {
@@ -104,41 +66,22 @@ else
 $a = "Вы вошли на сайт, как ".$_SESSION['login'];
 
     //bootstrap menu
-   echo "<html>
-    <div class=\"container\">
-     <div class=\"row\">
-         <h2>Главная страница</h2>
-         $a
-         <div class=\"navbar navbar-inverse\">
-             <div class=\"container\">
-                 <div class=\"navbar-header\">
-                 </div>
-                 <div class=\"collapse navbar-collapse\" id=\"responsiv-menu\">
-                     <ul class=\"nav navbar-nav\">
-                      <li> <br><a href=\"pars.php\"><br>Погода</a>
-                      <li> <br><a href=\"feedback.php\"><br>Обратная связь</a>
-                      <li> <br><a href=\"list_feedback.php\"><br>Список отзывов</a>
-                </ul> 
-                  </div>
-                  </div>
-                  </div>
-                  <form><br>
-                  <!--Форма выхода пользователя-->
-                   <input type=\"submit\" name=\"submitData\" value=\"Выход\" >
-                   </form>
-                  </div>
-                  </div>
-    </html>";
+    $array2=[['index.php','Главная страница'],['pars.php', 'Погода'],['feedback.php','Обратная связь'],['list_feedback.php','Список отзывов']];
+    echo menu($array2);
+    echo "
+               
+               
+           <h3> $a</h3>
+                <form>
+    <button type=\"submit\" class=\"btn btn-danger\">Выйти</button>
+</form>";
 
 
-
-   /* echo "<br><a href=\"pars.php\"><br>Погода</a>";
-    echo "<br><a href=\"feedback.php\">Обратная связь</a>";
-    echo "<br><a href=\"list_feedback.php\">Список отзывов</a>";*/
 
 
 }
 ?>
+
 
 </body>
 </html>
